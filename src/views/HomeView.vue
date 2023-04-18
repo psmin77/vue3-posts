7d28de415c52821a98ec519914a3c6bfcbf76e0a
Seongmin <chr92@naver.com>
2023-04-18 13:23:47 +0900

    [Section2] Vue Router

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<h1>Home View</h1>
+		<p>{{ $route.path }}</p>
+		<p>{{ $route.name }}</p>
+		<button class="btn btn-primary" @click="goAboutPage">ABOUT으로 이동</button>
+	</div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goAboutPage = () => {
+	router.push('/about');
+};
+</script>
+
+<style></style>
