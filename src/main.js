5574c03acf483668d5a206fd56865fc9a7c813fb
Seongmin <chr92@naver.com>
2023-05-24 17:55:09 +0900

    [Section9] Pinia - basic

--- src/main.js
+++ src/main.js
@@ -6,8 +6,7 @@ import router from '@/router';
 import person from './plugins/person';
 import globalDirectives from './plugins/global-directives';
 import dayjs from './plugins/dayjs';
-// import globalComponents from './plugins/global-components';
-// import focus from '@/directives/focus.js';
+import { createPinia } from 'pinia';
 
 const app = createApp(App);
 // app.use(globalComponents);
@@ -16,12 +15,7 @@ app.use(globalDirectives);
 app.use(dayjs);
 app.use(person);
 app.use(router);
+app.use(createPinia());
 app.mount('#app');
 
 import 'bootstrap/dist/js/bootstrap.js';
-
-// console.log('MODE: ' + import.meta.env.MODE);
-// console.log('BASE_URL: ' + import.meta.env.BASE_URL);
-// console.log('PROD: ' + import.meta.env.PROD);
-// console.log('DEV: ' + import.meta.env.DEV);
-// console.log('VITE_APP_API_URL: ' + import.meta.env.VITE_APP_API_URL);
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,14 +6,26 @@
 		<button class="btn btn-primary" @click="$router.push('/')">
 			HOME으로 이동
 		</button>
+		<h3>Store</h3>
+		<p>counter: {{ counter }}</p>
+		<p>doubleCount: {{ doubleCount }}</p>
+		<p>doubleCountPlusOne: {{ doubleCountPlusOne }}</p>
+		<button @click="increment()">Click!</button>
 	</div>
 </template>
 
 <script setup>
 import { useRoute } from 'vue-router';
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
 
 const route = useRoute();
 console.log('route.path: ', route.path);
+
+const store = useCounterStore();
+const { counter, doubleCount, doubleCountPlusOne } = storeToRefs(store);
+const { increment } = store;
+counter.value = 100;
 </script>
 
 <style></style>
